226e69920ee6f0720565b2e4a39b78a967668e8e
Nghia <20120536@student.hcmus.edu.vn>
2021-06-18 19:29:40 +0700

    TF has joined the chat

---
+++ TF.cpp
@@ -0,0 +1,67 @@
+#include "TF.h"
+
+void TFListInit(TF_list &List)
+{
+	List.size = 0;
+	List.capacity = 100;
+	List.arrNorm = (TF*)malloc(100 * sizeof(TF));
+	List.arrTele = (TF*)malloc(100 * sizeof(TF));
+}
+
+void addTF(TF_list &List, TF data, bool isTelex)
+{
+	if (List.size == List.capacity) {
+		List.capacity += 100;
+		List.arrNorm = (TF*)realloc(List.arrNorm, List.capacity * sizeof(TF));
+		List.arrTele = (TF*)realloc(List.arrTele, List.capacity * sizeof(TF));
+	}
+
+	if (isTelex)
+		List.arrTele[List.size++] = data;
+	else
+		List.arrNorm[List.size++] = data;
+}
+
+void LoadTFList(char *filename, TF_list& List)
+{
+	FILE* fr = fopen(filename, "rb");
+	if (!fr) {
+		fprintf(stderr, "Cannot read TF list!\n");
+		return;
+	}
+
+	FreeTFList(List);
+	fread(&List.capacity, sizeof(int), 1, fr);
+	fread(&List.size, sizeof(int), 1, fr);
+	fread(List.arrNorm, sizeof(TF), List.size, fr);
+	fread(List.arrTele, sizeof(TF), List.size, fr);
+
+	fclose(fr);
+}
+
+void SaveTFList(char *filename, TF_list List)
+{
+	FILE* fw = fopen(filename, "wb");
+	if (!fw) {
+		fprintf(stderr, "Cannot save TF list!\n");
+		return;
+	}
+
+
+	fwrite(&List.capacity, sizeof(int), 1, fw);
+	fwrite(&List.size, sizeof(int), 1, fw);
+	fwrite(List.arrNorm, sizeof(TF), List.size, fw);
+	fwrite(List.arrTele, sizeof(TF), List.size, fw);
+
+	fclose(fw);
+}
+
+void FreeTFList(TF_list &List)
+{
+	free(List.arrNorm);
+	free(List.arrTele);
+	List.arrNorm = nullptr;
+	List.arrTele = nullptr;
+	List.capacity = 0;
+	List.size = 0;
+}
--- TF.h
+++ TF.h
@@ -6,3 +6,16 @@ struct TF {
     double val;
     int count;
 };
+
+struct TF_list {
+	int size;
+	int capacity;
+	TF* arrNorm;
+	TF* arrTele;
+};
+
+void TFListInit(TF_list&);
+void addTF(TF_list&, TF, bool);
+void LoadTFList(char*, TF_list&);
+void SaveTFList(char*, TF_list);
+void FreeTFList(TF_list&);
